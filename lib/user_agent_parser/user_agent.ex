478b6a09c7acba16e077a73bda45625855cbbbd8
Sean Callan <seancallan@gmail.com>
2016-11-18 19:33:49 -0700

    Finalize user agent parsing

--- lib/user_agent_parser/user_agent.ex
+++ lib/user_agent_parser/user_agent.ex
@@ -3,8 +3,6 @@ defmodule UserAgentParser.UserAgent do
   User Agent struct and helper methods
   """
 
-  defstruct [:device, :family, :os, :version]
-
   @doc """
   Display the UserAgent as a string
 
@@ -12,17 +10,22 @@ defmodule UserAgentParser.UserAgent do
 
     iex> version = %UserAgentParser.Version{major: "1", minor: "2", patch: "3", patch_minor: "4"}
     iex> agent = %UserAgentParser.UserAgent{family: "Family", version: version}
-    iex> UserAgentParser.UserAgent.to_string(agent)
+    iex> to_string(agent)
     "Family 1.2.3.4"
 
     iex> agent = %UserAgentParser.UserAgent{family: "Family"}
-    iex> UserAgentParser.UserAgent.to_string(agent)
+    iex> to_string(agent)
     "Family"
 
     iex> agent = %UserAgentParser.UserAgent{}
-    iex> UserAgentParser.UserAgent.to_string(agent)
+    iex> to_string(agent)
     "Other"
   """
+
+  defstruct [:device, :family, :os, :version]
+end
+
+defimpl String.Chars, for: UserAgentParser.UserAgent do
   def to_string(%{family: family, version: nil}), do: family_name(family)
   def to_string(%{family: family, version: version}), do: "#{family_name(family)} #{version}"
 
--- test/user_agent_parser/parsers/user_agent_test.exs
+++ test/user_agent_parser/parsers/user_agent_test.exs
@@ -2,15 +2,16 @@ defmodule UserAgentParser.Parsers.UserAgentTest do
   use ExUnit.Case
 
   alias UserAgentParser.Parsers.UserAgent, as: Parser
-  alias UserAgentParser.{UserAgent, Storage, Version}
+  alias UserAgentParser.{UserAgent, Version}
 
   @user_string "Mozilla/5.0 (Windows; U; en-US) AppleWebKit/531.9 (KHTML, like Gecko) AdobeAIR/2.5.1"
+  @pattern [regex: ~r/\b(MobileIron|Crosswalk|AdobeAIR|FireWeb|Jasmine|ANTGalio|Midori|Fresco|Lobo|PaleMoon|Maxthon|Lynx|OmniWeb|Dillo|Camino|Demeter|Fluid|Fennec|Epiphany|Shiira|Sunrise|Spotify|Flock|Netscape|Lunascape|WebPilot|NetFront|Netfront|Konqueror|SeaMonkey|Kazehakase|Vienna|Iceape|Iceweasel|IceWeasel|Iron|K-Meleon|Sleipnir|Galeon|GranParadiso|Opera Mini|iCab|NetNewsWire|ThunderBrowse|Iris|UP\.Browser|Bunjalloo|Google Earth|Raven for Mac|Openwave|MacOutlook)\/(\d+)\.(\d+)\.(\d+)/]
+  @match ["AdobeAIR/2.5.1", "AdobeAIR", "2", "5", "1"]
 
   test "parses user agent information" do
-    {[pattern|_], _, _} = Storage.list
-
-    result = Parser.parse({pattern, ["AdobeAIR/2.5.1", "AdobeAIR", "2", "5", "1"]})
+    result  = Parser.parse({@pattern, @match})
     version = %Version{major: "2", minor: "5", patch: "1"}
+
     assert %UserAgent{family: "AdobeAIR", version: ^version} = result
   end
 end
