fbe70f6b9b913bc6db7a2682eb59fe4a5eb190ce
Nathan Youngman <nathan.youngman@copiawealthstudios.com>
2025-06-10 12:29:17 -0600

    fix: [#108] Remove module attribute for Erlang 28 (#112)

--- test/ua_parser/parsers/version_test.exs
+++ test/ua_parser/parsers/version_test.exs
@@ -4,15 +4,16 @@ defmodule UAParser.Parsers.VersionTest do
   alias UAParser.Parsers.Version, as: Parser
   alias UAParser.Version
 
-  @pattern [
-    regex: ~r/((?:Mac ?|; )OS X)[\s\/](?:(\d+)[_.](\d+)(?:[_.](\d+))?|Mach-O)/,
-    os_replacement: "Mac OS X"
-  ]
   @match ["Mac OS X", "10", "5", "7"]
   @replacement_keys [:not_used, :not_used, :not_used, :not_used]
 
   test "parses device information" do
-    result = Parser.parse({@pattern, @match}, @replacement_keys)
+    pattern = [
+      regex: ~r/((?:Mac ?|; )OS X)[\s\/](?:(\d+)[_.](\d+)(?:[_.](\d+))?|Mach-O)/,
+      os_replacement: "Mac OS X"
+    ]
+
+    result = Parser.parse({pattern, @match}, @replacement_keys)
     assert %Version{major: "10", minor: "5", patch: "7"} = result
   end
 end
