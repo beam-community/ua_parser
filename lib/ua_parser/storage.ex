f38b886bc70dc60388541bce0dbe7de27f0844ab
Jeff Sandberg <jeff@pdx.su>
2025-12-08 10:42:52 -0700

    Refactor storage to only do what's needed

--- lib/ua_parser/storage.ex
+++ lib/ua_parser/storage.ex
@@ -15,11 +15,9 @@ defmodule UAParser.Storage do
   """
   @spec load_table() :: :ok
   def load_table do
-    {ua_patterns, os_patterns, device_patterns} = read_from_yaml()
+    patterns = read_from_yaml()
 
-    simple_put(:ua_patterns, ua_patterns)
-    simple_put(:os_patterns, os_patterns)
-    simple_put(:device_patterns, device_patterns)
+    simple_put(patterns)
 
     :ok
   end
@@ -33,45 +31,23 @@ defmodule UAParser.Storage do
   end
 
   @doc """
-  Retrieves the list of user agent patterns from persistent_term.
-  """
-  @spec ua_patterns() :: term()
-  def ua_patterns, do: simple_get(:ua_patterns)
-
-  @doc """
-  Retrieves the list of operating system patterns from persistent_term.
-  """
-  @spec os_patterns() :: term()
-  def os_patterns, do: simple_get(:os_patterns)
-
-  @doc """
-  Retrieves the list of device patterns from persistent_term.
-  """
-  @spec device_patterns() :: term()
-  def device_patterns, do: simple_get(:device_patterns)
-
-  @doc """
   Returns a tuple containing all three pattern lists:
   `{user_agent_patterns, os_patterns, device_patterns}`.
   """
   @spec list() :: {term(), term(), term()}
   def list do
-    {ua_patterns(), os_patterns(), device_patterns()}
+    simple_get()
   end
 
-  defp simple_get(key) do
-    key
-    |> key()
-    |> :persistent_term.get()
+  defp simple_get do
+    :persistent_term.get(key())
   end
 
-  defp simple_put(key, value) do
-    key
-    |> key()
-    |> :persistent_term.put(value)
+  defp simple_put(value) do
+    :persistent_term.put(key(), value)
   end
 
-  defp key(name) do
-    {__MODULE__, name}
+  defp key do
+    {__MODULE__, :patterns}
   end
 end
